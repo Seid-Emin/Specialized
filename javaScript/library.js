ef6f39716cf5fbdeff338833fbb857c1201b6486
Seid Saydahmed <seid.saydahmed@gmail.com>
2019-12-12 02:51:56 +0200

    commit

--- javaScript/library.js
+++ javaScript/library.js
@@ -1,3 +1,8 @@
+// variables
+
+var up = $('.thumbsUp-rating-count');
+var down = $('.thumbsDown-rating-count')
+
 const pageNames = ['index', 'bikes', 'trail', 'downhill', 'sworks', 'StumpJumperPro29', 'StumpJumperExpert29', 'StumpJumperExpert27_5', 'FuseExpert29', 'FuseExpert27', 'DemoExpert29', 'DemoRace29', 'DemoAlloy27_5', 'SWorksDemo8', 'SWorksDemoEpicEVO', 'SWorksStumpJumper29', 'SWorksStumpJumper27_5', 'SWorksDemo8Frame'];
 const bikesPagesOnly = ['StumpJumperPro29', 'StumpJumperExpert29', 'StumpJumperExpert27_5', 'FuseExpert29', 'FuseExpert27', 'DemoExpert29', 'DemoRace29', 'DemoAlloy27_5', 'SWorksDemo8', 'SWorksDemoEpicEVO', 'SWorksStumpJumper29', 'SWorksStumpJumper27_5', 'SWorksDemo8Frame'];
 
@@ -22,12 +27,11 @@ let DemoRace29 = new bikesInfo('DemoRace 29', 'shop_individual_bikes/downhill/im
 let DemoExpert29 = new bikesInfo('StumpJumper Expert 29', 'shop_individual_bikes/downhill/img/DEMO-RACE-29[2]_dot.svg', '2000', '0', '0', 's');
 let DemoAlloy27_5 = new bikesInfo('DemoAlloy 27_5', 'shop_individual_bikes/downhill/img/DemoAlloy29[3]_dot.svg', '2000', '0', '0', 's');
 
-// S-Works Bikes
-// let S - WorksDemo8 = new bikesInfo('StumpJumper Pro 29', 'shop_individual_bikes/s-works/img/S-WorksDemo8[1]_dot.svg', '7800', '0', '0', 's');
-// let StumpJumperExpert29 = new bikesInfo('StumpJumper Expert 29', 'shop_individual_bikes/s-works/img/SJ-EX29-27.d14557fd[2],[3].svg', '2000', '0', '0', 's');
-// let StumpJumperExpert27_5 = new bikesInfo('StumpJumper Expert 27_5', 'shop_individual_bikes/s-works/img/SJ-EX29-27.d14557fd[2],[3].svg', '2000', '0', '0', 's');
-// let FuseExpert29 = new bikesInfo('FuseExpert 29', 'shop_individual_bikes/s-works/img/Fuse Expert 29[4].svg', '1100', '0', '0', 's');
-// let FuseExpert27_5 = new bikesInfo('FuseExpert 27_5', 'shop_individual_bikes/s-works/img/Fuse Expert 27[5].svg', '1000', '0', '0', 's');
+// S - Works Bikes
+let SWorksDemo8 = new bikesInfo('S-Works Demo 8', 'shop_individual_bikes/s-works/img/S-WorksDemo8[1]_dot.svg', '7500', '0', '0', 's');
+let SWorksDemoEpicEVO = new bikesInfo('S-Works Demo Epic EVO', 'shop_individual_bikes/s-works/img/S-WorksDemoEpicEVO[2]_dot.svg', '8400', '0', '0', 's');
+let SWorksStumpJumper29 = new bikesInfo('S-Works StumpJumper 29', 'shop_individual_bikes/s-works/img/S-WorksStumpJumper29[3]_[4]_dot.svg', '7400', '0', '0', 's');
+let SWorksStumpJumper27_5 = new bikesInfo('S-Works StumpJumper 27.5', 'shop_individual_bikes/s-works/img/S-WorksStumpJumper29[3]_[4]_dot.svg', '7400', '0', '0', 's');
+let SWorksDemo8Frame = new bikesInfo('S-Works Demo 8 Frame', 'shop_individual_bikes/s-works/img/S-WorksDemo8Frame[5]_dot.svg', '1000', '0', '0', 's');
 
-
-let arrBikesInfo = [StumpJumperPro29, StumpJumperExpert29, StumpJumperExpert27_5, FuseExpert29, FuseExpert27_5, DemoRace29, DemoExpert29, DemoAlloy27_5];
+let arrBikesInfo = [StumpJumperPro29, StumpJumperExpert29, StumpJumperExpert27_5, FuseExpert29, FuseExpert27_5, DemoRace29, DemoExpert29, DemoAlloy27_5, SWorksDemo8, SWorksDemoEpicEVO, SWorksStumpJumper29, SWorksStumpJumper27_5, SWorksDemo8Frame];
--- javaScript/localStorage.js
+++ javaScript/localStorage.js
@@ -5,7 +5,7 @@
 for (let i = 0; i < arrBikesInfo.length; i++) {
     if (localStorage.getItem(localStorage.key(i)) === null) {
         // let NewBikesInfo += bikesPagesOnly[i].concat('Info');
-        window.localStorage.setItem(`${bikesPagesOnly[i]}`, JSON.stringify(arrBikesInfo[i]));
+        localStorage.setItem(`${bikesPagesOnly[i]}`, JSON.stringify(arrBikesInfo[i]));
 
     }
 };
@@ -13,14 +13,38 @@ for (let i = 0; i < bikesPagesOnly.length; i++) {
     var currentPageName = bikesPagesOnly[i];
     var fullPageName = currentPageName.concat('.html');
     if (window.location.href.indexOf(fullPageName) > -1) {
-        var PageName = fullPageName;
-        var currentItem = JSON.parse(localStorage.getItem(`${currentPageName}`));
+        var pageName = fullPageName;
+        var page = currentPageName;
+        var currentItem = JSON.parse(localStorage.getItem(currentPageName));
         $('.thumbsUp-rating-count').text(currentItem.thumbsUp);
         $('.thumbsDown-rating-count').text(currentItem.thumbsDown);
-        var upClicked = 15;
-        currentItem.thumbsUp = upClicked;
-        console.log(currentItem);
-        $('.thumbsUp-rating-count').text(currentItem.thumbsUp);
+        if (currentItem.thumbsUp > 0) {
+            $(".thumbsUp-rating-count").addClass("thumbsUp-active");
+        }
+        if (currentItem.thumbsDown > 0) {
+            $(".thumbsDown-rating-count").addClass("thumbsDown-active");
+        }
+        // var upClicked = 15;
+        // currentItem.thumbsUp = upClicked;
+        // console.log(currentItem);
+        // $('.thumbsUp-rating-count').text(currentItem.thumbsUp);
     }
 }
-console.log(currentItem.thumbsUp);
+// Thumb Up
+$('.rating-icon_thumbsUp').click(function () {
+    let currentUpCount = parseInt(currentItem.thumbsUp);
+    currentItem.thumbsUp = ++currentUpCount;
+    currentUpCount = increase("Up", currentUpCount);;
+});
+
+// Thumb Up
+$('.rating-icon_thumbsDown').click(function () {
+    let currentDownCount = parseInt(currentItem.thumbsDown);
+    currentItem.thumbsDown = ++currentDownCount;
+    currentDownCount = increase("Down", currentDownCount);
+});
+
+function increase(e, o) {
+    $('.thumbs' + e + '-rating-count').text(o);
+    localStorage.setItem(page, JSON.stringify(currentItem));
+}
