03b9ba2c0ad54a5ab7038b41fafa1bce555bd8a1
Seid Saydahmed <seid.saydahmed@gmail.com>
2019-12-16 01:40:26 +0200

    commit

--- javaScript/library.js
+++ javaScript/library.js
@@ -6,7 +6,7 @@
 const pageNames = ['index', 'bikes', 'trail', 'downhill', 'sworks', 'StumpJumperPro29', 'StumpJumperExpert29', 'StumpJumperExpert27_5', 'FuseExpert29', 'FuseExpert27', 'DemoExpert29', 'DemoRace29', 'DemoAlloy27_5', 'SWorksDemo8', 'SWorksDemoEpicEVO', 'SWorksStumpJumper29', 'SWorksStumpJumper27_5', 'SWorksDemo8Frame'];
 const bikesPagesOnly = ['StumpJumperPro29', 'StumpJumperExpert29', 'StumpJumperExpert27_5', 'FuseExpert29', 'FuseExpert27', 'DemoExpert29', 'DemoRace29', 'DemoAlloy27_5', 'SWorksDemo8', 'SWorksDemoEpicEVO', 'SWorksStumpJumper29', 'SWorksStumpJumper27_5', 'SWorksDemo8Frame'];
 
-function bikesInfo(name, color, price, thumbsUp, thumbsDown, size, type, partNum, qty, html) {
+function bikesInfo(name, color, price, thumbsUp, thumbsDown, size, type, partNum, qty, html, total) {
     this.name = name;
     this.color = color;
     this.price = price;
@@ -17,25 +17,26 @@ function bikesInfo(name, color, price, thumbsUp, thumbsDown, size, type, partNum
     this.partNum = partNum;
     this.qty = qty;
     this.html = html;
+    this.total = total;
 }
 
 // Trail Bikes
-let StumpJumperPro29 = new bikesInfo('StumpJumper Pro 29', 'shop_individual_bikes/trail/img/StumpJumperPro29-Hero_[1].svg', '3300', '0', '0', 's', 'trail', '94519-0001', '1', 'StumpJumperPro29.html');
-let StumpJumperExpert29 = new bikesInfo('StumpJumper Expert 29', 'shop_individual_bikes/trail/img/SJ-EX29-27.d14557fd[2],[3].svg', '2000', '0', '0', 's', 'trail', '94519-0002', '1', 'StumpJumperExpert29.html');
-let StumpJumperExpert27_5 = new bikesInfo('StumpJumper Expert 27_5', 'shop_individual_bikes/trail/img/SJ-EX29-27.d14557fd[2],[3].svg', '2000', '0', '0', 's', 'trail', '94519-0003', '1', 'StumpJumperExpert27_5.html');
-let FuseExpert29 = new bikesInfo('FuseExpert 29', 'shop_individual_bikes/trail/img/Fuse Expert 29[4].svg', '1100', '0', '0', 's', '94519-0004', '1', 'FuseExpert29.html');
-let FuseExpert27_5 = new bikesInfo('FuseExpert 27_5', 'shop_individual_bikes/trail/img/Fuse Expert 27[5].svg', '1000', '0', '0', 's', '94519-0005', '1', 'FuseExpert27_5.html');
+let StumpJumperPro29 = new bikesInfo('StumpJumper Pro 29', 'shop_individual_bikes/trail/img/StumpJumperPro29-Hero_[1].svg', '3300', '0', '0', 's', 'trail', '94519-0001', '1', 'StumpJumperPro29.html', '0');
+let StumpJumperExpert29 = new bikesInfo('StumpJumper Expert 29', 'shop_individual_bikes/trail/img/SJ-EX29-27.d14557fd[2],[3].svg', '2200', '0', '0', 's', 'trail', '94519-0002', '1', 'StumpJumperExpert29.html', '0');
+let StumpJumperExpert27_5 = new bikesInfo('StumpJumper Expert 27_5', 'shop_individual_bikes/trail/img/SJ-EX29-27.d14557fd[2],[3].svg', '2000', '0', '0', 's', 'trail', '94519-0003', '1', 'StumpJumperExpert27_5.html', '0');
+let FuseExpert29 = new bikesInfo('FuseExpert 29', 'shop_individual_bikes/trail/img/Fuse Expert 29[4].svg', '1100', '0', '0', 's', '94519-0004', '1', 'FuseExpert29.html', '0');
+let FuseExpert27_5 = new bikesInfo('FuseExpert 27_5', 'shop_individual_bikes/trail/img/Fuse Expert 27[5].svg', '1000', '0', '0', 's', '94519-0005', '1', 'FuseExpert27_5.html', '0');
 
 // Downhill Bikes
-let DemoRace29 = new bikesInfo('DemoRace 29', 'shop_individual_bikes/downhill/img/DEMO-RACE-29[2]_dot.svg', '4500', '0', '0', 's', 'downhill', '94519-0006', '1', 'DemoRace29.html');
-let DemoExpert29 = new bikesInfo('StumpJumper Expert 29', 'shop_individual_bikes/downhill/img/DEMO-RACE-29[2]_dot.svg', '2000', '0', '0', 's', 'downhill', '94519-0007', '1', 'DemoExpert29.html');
-let DemoAlloy27_5 = new bikesInfo('DemoAlloy 27_5', 'shop_individual_bikes/downhill/img/DemoAlloy29[3]_dot.svg', '2000', '0', '0', 's', 'downhill', '94519-0008', '1', 'DemoAlloy27_5.html');
+let DemoRace29 = new bikesInfo('DemoRace 29', 'shop_individual_bikes/downhill/img/DEMO-RACE-29[2]_dot.svg', '4500', '0', '0', 's', 'downhill', '94519-0006', '1', 'DemoRace29.html', '0');
+let DemoExpert29 = new bikesInfo('StumpJumper Expert 29', 'shop_individual_bikes/downhill/img/DEMO-RACE-29[2]_dot.svg', '2000', '0', '0', 's', 'downhill', '94519-0007', '1', 'DemoExpert29.html', '0');
+let DemoAlloy27_5 = new bikesInfo('DemoAlloy 27_5', 'shop_individual_bikes/downhill/img/DemoAlloy29[3]_dot.svg', '2000', '0', '0', 's', 'downhill', '94519-0008', '1', 'DemoAlloy27_5.html', '0');
 
 // S - Works Bikes
-let SWorksDemo8 = new bikesInfo('S-Works Demo 8', 'shop_individual_bikes/s-works/img/S-WorksDemo8[1]_dot.svg', '7500', '0', '0', 's', 's-works', '94519-0009', '1', 'SWorksDemo8.html');
-let SWorksDemoEpicEVO = new bikesInfo('S-Works Demo Epic EVO', 'shop_individual_bikes/s-works/img/S-WorksDemoEpicEVO[2]_dot.svg', '8400', '0', '0', 's', 's-works', '94519-0010', '1', 'SWorksDemoEpicEVO.html');
-let SWorksStumpJumper29 = new bikesInfo('S-Works StumpJumper 29', 'shop_individual_bikes/s-works/img/S-WorksStumpJumper29[3]_[4]_dot.svg', '7400', '0', '0', 's', 's-works', '94519-0011', '1', 'SWorksStumpJumper29.html');
-let SWorksStumpJumper27_5 = new bikesInfo('S-Works StumpJumper 27.5', 'shop_individual_bikes/s-works/img/S-WorksStumpJumper29[3]_[4]_dot.svg', '7400', '0', '0', 's', 's-works', '94519-0012', '1', 'SWorksStumpJumper27_5.html');
-let SWorksDemo8Frame = new bikesInfo('S-Works Demo 8 Frame', 'shop_individual_bikes/s-works/img/S-WorksDemo8Frame[5]_dot.svg', '1000', '0', '0', 's', 's-works', '94519-0013', '1', 'SWorksDemo8Frame.html');
+let SWorksDemo8 = new bikesInfo('S-Works Demo 8', 'shop_individual_bikes/s-works/img/S-WorksDemo8[1]_dot.svg', '7500', '0', '0', 's', 's-works', '94519-0009', '1', 'SWorksDemo8.html', '0');
+let SWorksDemoEpicEVO = new bikesInfo('S-Works Demo Epic EVO', 'shop_individual_bikes/s-works/img/S-WorksDemoEpicEVO[2]_dot.svg', '8400', '0', '0', 's', 's-works', '94519-0010', '1', 'SWorksDemoEpicEVO.html', '0');
+let SWorksStumpJumper29 = new bikesInfo('S-Works StumpJumper 29', 'shop_individual_bikes/s-works/img/S-WorksStumpJumper29[3]_[4]_dot.svg', '7400', '0', '0', 's', 's-works', '94519-0011', '1', 'SWorksStumpJumper29.html', '0');
+let SWorksStumpJumper27_5 = new bikesInfo('S-Works StumpJumper 27.5', 'shop_individual_bikes/s-works/img/S-WorksStumpJumper29[3]_[4]_dot.svg', '7400', '0', '0', 's', 's-works', '94519-0012', '1', 'SWorksStumpJumper27_5.html', '0');
+let SWorksDemo8Frame = new bikesInfo('S-Works Demo 8 Frame', 'shop_individual_bikes/s-works/img/S-WorksDemo8Frame[5]_dot.svg', '1000', '0', '0', 's', 's-works', '94519-0013', '1', 'SWorksDemo8Frame.html', '0');
 
 let arrBikesInfo = [StumpJumperPro29, StumpJumperExpert29, StumpJumperExpert27_5, FuseExpert29, FuseExpert27_5, DemoRace29, DemoExpert29, DemoAlloy27_5, SWorksDemo8, SWorksDemoEpicEVO, SWorksStumpJumper29, SWorksStumpJumper27_5, SWorksDemo8Frame];
--- javaScript/local_almost_working.js
+++ javaScript/local_almost_working.js
@@ -1,3 +1,13 @@
+// Still adding dublicate cartItems
+// In cart sincing same items value
+// delete update need to me implemented
+
+
+// check local storage empty?. If so create key
+// window.localStorage.setItem('StumpJumperPro29', JSON.stringify(StumpJumperPro29));
+// window.localStorage.setItem('StumpJumperExpert29', JSON.stringify(StumpJumperExpert29));
+// window.localStorage.setItem('StumpJumperExpert29', JSON.stringify(StumpJumperExpert29));
+
 // Set To LocalStorage
 function onLoad() {
     if (localStorage.getItem("addedItemsCounter") === null) {
@@ -8,28 +18,47 @@ function onLoad() {
         ProdCountCartIcon.innerHTML = `<p class="cartIconProdCount">${addedItemsCounter}</p>`;
     }
     if (localStorage.getItem("productsCounter") === null) {
-        localStorage.setItem("productsCounter", 0);
+        localStorage.setItem("productsCounter", 1);
     }
     if (localStorage.getItem("finalTotal") === null) {
         localStorage.setItem("finalTotal", 0);
     } else if (localStorage.getItem("finalTotal") != 0) {
         var finalTotal = localStorage.finalTotal;
         var totalCart = document.querySelector('.partTotalPrice');
-        if (totalCart) {
-            totalCart.innerHTML = `<p>$${(finalTotal/1000).toFixed(3)}.000</p>`;
-        }
+        totalCart.innerHTML = `<p>$${(finalTotal/1000).toFixed(3)}.000</p>`;
     }
-    window.addedItemsCounter = localStorage.addedItemsCounter;
-    window.productsCounter = localStorage.productsCounter;
-
     updateIconCounter();
+
+
+
+
 }
+var addedItemsCounter = localStorage.addedItemsCounter;
+var productsCounter = localStorage.productsCounter;
 
 for (var i = 0; i < arrBikesInfo.length; i++) {
     if (localStorage.getItem(localStorage.key(i)) === null) {
         localStorage.setItem(bikesPagesOnly[i], JSON.stringify(arrBikesInfo[i]));
     }
 }
+// Loop CartHolders
+// function loopCartHolders(inpName, inpSize, inpQty) {
+//     for (var i = 1; i < productsCounter; i++) {
+//         var key = "";
+//         key = "cartHolder" + i;
+//         // console.log(key);
+//         var getObject = JSON.parse(localStorage.getItem(key));
+//         var currentName = getObject[0].name;
+//         var currentSize = getObject[0].size;
+//         var currentQty = getObject[0].qty;
+//         if (currentName == inpName && currentSize == inpSize) {
+//             currentQty = inpQty;
+//             return inpQty;
+//         } else {
+//             productsCounter++;
+//         }
+//     }
+// }
 // set counter
 
 // Thumb Up
@@ -76,45 +105,36 @@ for (var i = 0; i < bikesPagesOnly.length; i++) {
     }
 }
 
-// Removes Dublicates while adding item to cardHolders
-function removeDublicats() {
-    for (var i = 0; i < productsCounter; i++) {
-        var key = "";
-        key = "cartHolder" + i;
-        var firstObj = JSON.parse(localStorage.getItem(key));
-        if (!firstObj) {
-            continue;
-        }
-        var firstObjName = firstObj[0].name;
-        var firstObjSize = firstObj[0].size;
-        for (var j = i + 1; j < productsCounter; j++) {
-            var nextKey = "";
-            nextKey = "cartHolder" + j;
-            var secondObj = JSON.parse(localStorage.getItem(nextKey));
-            if (!secondObj) {
-                continue;
-            }
-            var secondObjName = secondObj[0].name;
-            var secondObjSize = secondObj[0].size;
 
-            var compareName = firstObjName.localeCompare(secondObjName);
-            var compareSize = firstObjSize.localeCompare(secondObjSize);
-            if (compareName === 0 && compareSize === 0) {
+// function removeDublicats() {
+//     for (var i = 1; i < productsCounter - 1; i++) {
+//         var key = "";
+//         key = "cartHolder" + i;
+//         var firstObj = JSON.parse(localStorage.getItem(key));
+//         var firstObjName = firstObj[0].name;
+//         var firstObjSize = firstObj[0].size;
+//         for (var j = 2; j < productsCounter; j++) {
+//             var nextKey = "";
+//             nextKey = "cartHolder" + j;
+//             var secondObj = JSON.parse(localStorage.getItem(nextKey));
+//             var secondObjName = secondObj[0].name;
+//             var secondObjSize = secondObj[0].size;
 
-                localStorage.setItem(key, JSON.stringify(secondObj));
-                localStorage.removeItem(nextKey);
-                productsCounter -= 1;
-                localStorage.setItem("productsCounter", productsCounter);
-                updateIconCounter();
-                updateTotalAfterAdding();
-            }
-        }
-    }
-}
+//             var compareName = firstObjName.localeCompare(secondObjName);
+//             var compareSize = firstObjSize.localeCompare(secondObjSize);
+//             if (compareName === 0 && compareSize === 0) {
 
-// Delete item from cart and storage
+//                 localStorage.setItem(key, JSON.stringify(secondObj));
+//                 localStorage.removeItem(nextKey);
+//                 productsCounter -= 1;
+//                 localStorage.setItem("productsCounter", Number(productsCounter));
 
-
+//             }
+//         }
+//     }
+//     updateIconCounter();
+//     updateTotalAfterAdding()
+// }
 // Selectiong button to add Item in LocalStorage then to Card
 
 $('.bike-cart_add-to-cart').click(function () {
@@ -123,7 +143,7 @@ $('.bike-cart_add-to-cart').click(function () {
     var cartItemName = itemWrap.find('.bike-name').text();
     var cartItemSize = itemWrap.find('.bike-size :selected').val();
     // document.querySelector("")
-    var cartItemQty = itemWrap.find('.order-count_select').val();
+    var cartItemQty = itemWrap.find('.bike-qty_option').val();
 
     var itemName = cartItemName.replace(" ", "");
 
@@ -131,31 +151,47 @@ $('.bike-cart_add-to-cart').click(function () {
         itemName = itemName.replace(" ", "");
     }
     var currentBike = JSON.parse(localStorage.getItem(itemName));
+    // let productsCounter = Number(localStorage.productsCounter);
+    // itemName = itemName.concat('.html');
 
     currentBike.size = cartItemSize;
     currentBike.qty = cartItemQty;
     partImetPrice = currentBike.price;
     currentBike.total = Number(cartItemQty) * Number(partImetPrice);
 
+    // var cartPrice = currentBike.qty;
+    // var currentItemTotalPrice = cartItemQty * cartPrice;
     addedItemsCounter += Number(cartItemQty);
     localStorage.setItem(itemName, JSON.stringify(currentBike));
+    // console.log(itemName);
 
     var cartHolder = [];
     cartHolder.push(currentBike);
+    // console.log(currentBike);
 
     localStorage.setItem('cartHolder' + productsCounter, JSON.stringify(cartHolder));
     productsCounter++;
     localStorage.setItem("productsCounter", productsCounter);
     localStorage.setItem("addedItemsCounter", Number(addedItemsCounter));
-
-    removeDublicats();
-    updateIconCounter(addedItemsCounter);
-
+    // var a = JSON.parse(localStorage.getItem(cartHolder));
+    // cartHolder[0];
+    // console.log(cartHolder);
+    // removeDublicats();
+    // updateIconCounter(addedItemsCounter);
+    // var total = 0;
+    // for (var i = 1; i < productsCounter; i++) {
+    //     var key = "";
+    //     key = "cartHolder" + i;
+    //     var getObjectChange = JSON.parse(localStorage.getItem(key));
+    //     var objPrice = getObjectChange[0].price;
+    //     total += Number(objPrice);
+    // }
+    // updateTotal(total);
 });
 
 function updateIconCounter() {
     var cartItemsCounter = 0;
-    for (var i = 0; i < productsCounter; i++) {
+    for (var i = 1; i < productsCounter; i++) {
         var key = "";
         key = "cartHolder" + i;
         var getObjectChange = JSON.parse(localStorage.getItem(key));
@@ -175,7 +211,7 @@ function updateTotal(t) {
 
 function updateTotalAfterAdding() {
     var total = 0;
-    for (var i = 0; i < productsCounter; i++) {
+    for (var i = 1; i < productsCounter; i++) {
         var key = "";
         key = "cartHolder" + i;
         var getObjectChange = JSON.parse(localStorage.getItem(key));
@@ -187,23 +223,25 @@ function updateTotalAfterAdding() {
     localStorage.setItem("finalTotal", total);
 }
 
-function updateProductsCounter() {
-    for (var i = 0; i < productsCounter; i++) {
-        var key = "";
-        key = "cartHolder" + i;
-        productsCounter = 0;
-        var getObjectChange = JSON.parse(localStorage.getItem(key));
-        if (!getObjectChange) {
-            productsCounter++;
-        }
-    }
-    localStorage.setItem("productsCounter", productsCounter);
-}
+
+// show added items
+// check Cart and add items
+
+// var getObject = JSON.parse(localStorage.getItem('cartHolder'));
+// console.log(getObject);
+
+// document.addEventListener('DOMContentLoaded', function(){
+
+// });
+
 
 function showCart() {
+
     if (JSON.parse(localStorage.getItem('cartHolder1')) == null) {
         var cartTable = document.querySelector('.bike-cart-table');
         var cartWrap = document.querySelector('.cart-table-wrap');
+        // console.log(getObject);
+
         var emptyCart = document.createElement("div");
         emptyCart.className = "cart-content-wrap-empty";
         emptyCart.innerHTML = `<p class="cart-content_title bolder">is Empty</p>
@@ -212,16 +250,50 @@ function showCart() {
         <a class="cart-header_contShopping" href="../shop/bikes.html">Continue Shopping</a>`;
         cartTable.insertBefore(emptyCart, cartWrap);
         $('.cart-table-wrap').hide();
+
+        // var content_title = document.createElement("p");
+        // content_title.className = "cart-content_title text-align-last";
+        // content_title.innerHTML = `<div class = "bolder">is Empty</div>`;
+
+        // var content_text = document.createElement("p");
+        // content_text.className = "cart-content_text text-align-last";
+        // content_text.innerHTML = `Nothin ' to see here.`;
+
+        // var content_text1 = document.createElement("p");
+        // content_text1.className = "cart-content_text text-align-last";
+        // content_text1.innerHTML = `Check out our bikes.`;
+
+        // var header_contShopping = document.createElement("a");
+        // header_contShopping.className = "cart-header_contShopping text-align-last";
+        // header_contShopping.href = "../shop/bikes.html";
+        // header_contShopping.innerHTML = "Continue Shopping";
+
+        // Insert them
+        // cart.insertBefore(content_title, totalContainer);
+        // cart.insertBefore(content_text, totalContainer);
+        // cart.insertBefore(content_text1, totalContainer);
+        // cart.insertBefore(header_contShopping, totalContainer);
+
+
     } else {
-        for (let j = 0; j < productsCounter; j++) {
+        // var getObject = JSON.parse(localStorage.getItem('cartHolder1'));
+        for (let j = 1; j < productsCounter; j++) {
             let key = "";
             key = "cartHolder" + j;
+            // console.log(key);
+
+
             var getObject = JSON.parse(localStorage.getItem(key));
+            // console.log(getObject);
             key.replace('cartHolder', 'cartHolder' + j)
             $('.cart-content-wrap-empty').hide();
             for (let i = 0; i < 1; i++) {
                 var cart = document.querySelector('.cart-section-title-table');
                 var totalContainer = document.querySelector('.totalContainer');
+
+
+
+
                 var cartProducts = document.createElement("tr");
                 cartProducts.className = "cart-section-category";
                 cartProducts.innerHTML = `<td class="cart-section-category-title partNum addedItem">${getObject[i].partNum}</td>
@@ -231,7 +303,7 @@ function showCart() {
             <td class="cart-section-category-title partQty addedItem uppercase align-cent"> <input type="text" value="${getObject[i].qty}" class="bike-qty_option"></td>
             <td class="cart-section-category-title partPrice addedItem">$${((getObject[i].total)/1000).toFixed(3)}.000</td>
             <td class="cart-section-category-title-btn partDelBtn addedItem"><input
-                    type="button" class="deleteItem cartHolder${j}" id="cartHolder${j}""></input></td>`;
+                    type="button" class="deleteItem deleteItem${j}"></input></td>`;
                 cart.insertBefore(cartProducts, totalContainer);
             }
         }
@@ -239,73 +311,21 @@ function showCart() {
     // Delete Item From Cart
     $('.partDelBtn').click(function () {
 
-        var classNameDelBtn = $(this).find('.deleteItem').attr('id');
-
-        for (var i = 0; i < productsCounter; i++) {
-            var key = "";
-            key = "cartHolder" + i;
-            var firstObj = JSON.parse(localStorage.getItem(key));
-            if (classNameDelBtn == key) {
-                for (var j = i + 1; j < productsCounter; j++) {
-                    var nextKey = "";
-                    nextKey = "cartHolder" + j;
-                    localStorage.removeItem(firstObj);
-                    var secondObj = JSON.parse(localStorage.getItem(nextKey));
-                    if (secondObj != null) {
-                        localStorage.setItem(key, JSON.stringify(secondObj));
-                    } else {
-                        var lastKey = "";
-                        lastKey = "cartHolder" + productsCounter;
-                        localStorage.removeItem(lastKey);
-                    }
-
-
-
-                    var nextNextKey = "";
-                    nextNextKey = "cartHolder" + `${j+1}`;
-                    var thurdObj = JSON.parse(localStorage.getItem(nextNextKey));
-                    localStorage.setItem(nextKey, JSON.stringify(thurdObj));
-                    localStorage.removeItem(nextNextKey);
-                }
-            }
-
-        }
         var name = $(this).parent().find('.whyNot').attr("href").replace('.html', '');
         $(this).parent().remove();
-        // getObject[0] = "";
-        // var index = getObject.indexOf(name);
-        // if (index > -1) {
-        //     getObject.splice(index, 1);
-        //     localStorage.setItem(getObject, JSON.stringify(getObject));
-        // }
+        getObject[0] = "";
+        // console.log(name);
 
-        productsCounter -= 1;
-        localStorage.setItem("productsCounter", productsCounter);
-        updateIconCounter();
-        updateTotalAfterAdding();
-        for (var j = i + 1; j < productsCounter; j++) {
-            var asd = "";
-            asd = "cartHolder" + j;
-            var dsa = JSON.parse(localStorage.getItem(asd));
-            if (!dsa) {
-                localStorage.removeItem(asd);
-            }
+        var index = getObject.indexOf(name);
+        // console.log(index);
+
+        if (index > -1) {
+            getObject.splice(index, 1);
+            localStorage.setItem(getObject, JSON.stringify(getObject));
+            // console.log(getObject);
         }
-        var checkItems = localStorage.addedItemsCounter;
-        if (checkItems == 0) {
-            localStorage.clear();
-            var cartTable = document.querySelector('.bike-cart-table');
-            var cartWrap = document.querySelector('.cart-table-wrap');
-            var emptyCart = document.createElement("div");
-            emptyCart.className = "cart-content-wrap-empty";
-            emptyCart.innerHTML = `<p class="cart-content_title bolder">is Empty</p>
-            <p class="cart-content_text">Nothin' to see here.</p>
-            <p class="cart-content_text">Check out our bikes.</p>
-            <a class="cart-header_contShopping" href="../shop/bikes.html">Continue Shopping</a>`;
-            cartTable.insertBefore(emptyCart, cartWrap);
-            $('.cart-table-wrap').hide();
-            $('.cart-content-wrap-empty').show();
-        }
+
+
     });
 
     // Change Product Qtity in CART
@@ -313,7 +333,7 @@ function showCart() {
         var currentName = $(this).parent().parent().find('.whyNot').text();
         var currentQty = $(this).val();
         var currentSize = $(this).parent().parent().find('.partSize').text();
-        for (var i = 0; i < productsCounter; i++) {
+        for (var i = 1; i < productsCounter; i++) {
             var key = "";
             key = "cartHolder" + i;
             var getObjectChange = JSON.parse(localStorage.getItem(key));
@@ -336,6 +356,5 @@ function showCart() {
         }
         updateIconCounter();
         updateTotalAfterAdding();
-        updateProductsCounter()
     });
 }
