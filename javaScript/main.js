8cf401f2dcc2e1837bdf8f3828df567d1049f622
Seid Saydahmed <seid.saydahmed@gmail.com>
2019-12-16 15:51:44 +0200

    commit

--- javaScript/main.js
+++ javaScript/main.js
@@ -247,7 +247,13 @@ $(document).ready(function () {
 
 
 
-
+    $('.carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
 
 
 });
